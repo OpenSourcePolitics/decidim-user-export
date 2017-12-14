feb34602512cd5327e673b6e1e549415568290e5
Ludivine Coipel <coipel.ludivine@gmail.com>
2017-12-14 17:01:50 +0100

    Add followers request

--- app/controllers/decidim/export/admin/export_controller.rb
+++ app/controllers/decidim/export/admin/export_controller.rb
@@ -41,16 +41,29 @@ module Decidim
         def users_to_export
           process_id = request.query_parameters[:process_id]
           if process_id
-            get_proposals_author(process_id).uniq
+            (get_proposals_author(process_id) + get_proposals_followers(process_id)).uniq
           else
             current_organization.users.uniq
           end
         end
 
         def get_proposals_author(process_id)
-          features = Decidim::Feature.where(manifest_name: :proposals, participatory_space: process_id)
-          proposals = Decidim::Proposals::Proposal.where(feature: features)
-          proposals.map(&:author)
+           get_proposals(process_id).map(&:author)
+        end
+
+        def get_features(process_id)
+          Decidim::Feature.where(manifest_name: :proposals, participatory_space: process_id)
+        end
+
+        def get_proposals(process_id)
+          proposals = Decidim::Proposals::Proposal.where(feature: get_features(process_id))
+        end
+
+        def get_proposals_followers(process_id)
+          followers_ids = []
+          get_proposals(process_id).each{|p| followers_ids << p.follows.where.not(decidim_user_id: nil).map(&:decidim_user_id)}
+          followers = Decidim::User.where(id: followers_ids.flatten)
+          followers
         end
       end
     end
