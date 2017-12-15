9ff85206af1bd224bcd3f81c2782c7b92fb0041c
Ludivine Coipel <coipel.ludivine@gmail.com>
2017-12-15 15:04:08 +0100

    Include assembly

--- app/controllers/decidim/export/admin/export_controller.rb
+++ app/controllers/decidim/export/admin/export_controller.rb
@@ -14,7 +14,7 @@ module Decidim
 
         def index
           authorize! :index, Decidim::Export
-          participatory_process
+          @participatory_spaces = participatory_spaces
         end
 
         def show
@@ -31,69 +31,76 @@ module Decidim
 
         private
 
-        def participatory_process
+        def participatory_spaces
           @participatory_process ||= Decidim::ParticipatoryProcess.where(organization: current_organization)
+          @assemblies = Decidim::Assembly.where(organization: current_organization) if Object.const_defined?('Decidim::Assembly') # Check if the gem Decidim::Assembly is active
+          @participatory_process + @assemblies
         end
+
         def export_data(data)
           Decidim::Export::Csv.new(data, [:name, :email]).export
         end
 
+        def participatory_space
+          space_id = request.query_parameters[:space_id]
+          space_type = request.query_parameters[:space_type].constantize
+          participatory_space = space_type.find(space_id)
+        end
+
         def users_to_export
-          process_id = request.query_parameters[:process_id]
-          if process_id
-            (get_proposals_author(process_id) +
-             get_proposals_followers(process_id) +
-             get_users_who_vote(process_id) +
-             get_attended_users(process_id) +
-             get_attended_users(process_id) +
-             get_comment_author(process_id) +
-             get_comment_followers(process_id)
+          if participatory_space
+            (get_proposals_author +
+             get_proposals_followers +
+             get_users_who_vote +
+             get_attended_users +
+             get_attended_users +
+             get_comment_author +
+             get_comment_followers
             ).uniq
           else
             current_organization.users.uniq
           end
         end
 
-        def get_proposals_author(process_id)
-           get_proposals(process_id).map(&:author)
+        def get_proposals_features
+          Decidim::Feature.where(manifest_name: :proposals, participatory_space: participatory_space)
         end
 
-        def get_proposals_features(process_id)
-          Decidim::Feature.where(manifest_name: :proposals, participatory_space: process_id)
+        def get_proposals
+          proposals = Decidim::Proposals::Proposal.where(feature: get_proposals_features)
         end
 
-
-        def get_proposals(process_id)
-          proposals = Decidim::Proposals::Proposal.where(feature: get_proposals_features(process_id))
+        def get_proposals_author
+           get_proposals.map(&:author)
         end
 
-        def get_meeting(process_id)
-          features = Decidim::Feature.where(manifest_name: :meetings, participatory_space: process_id)
+        def get_meeting
+          features = Decidim::Feature.where(manifest_name: :meetings, participatory_space: participatory_space)
           Decidim::Meetings::Meeting.where(feature: features)
         end
 
-        def get_attended_users(process_id)
-          get_meeting(process_id).map(&:registrations).flatten.map(&:user)
+        def get_attended_users
+          get_meeting.map(&:registrations).flatten.map(&:user)
         end
 
-        def get_users_who_vote(process_id)
-          users_ids = get_proposals(process_id).map(&:votes).flatten.map(&:author)
+        def get_users_who_vote
+          users_ids = get_proposals.map(&:votes).flatten.map(&:author)
         end
 
-        def get_proposals_followers(process_id)
+        def get_proposals_followers
           followers_ids = []
-          get_proposals(process_id).each{|p| followers_ids << p.follows.where.not(decidim_user_id: nil).map(&:decidim_user_id)}
+          get_proposals.each{|p| followers_ids << p.follows.where.not(decidim_user_id: nil).map(&:decidim_user_id)}
           followers = Decidim::User.where(id: followers_ids.flatten)
           followers
         end
 
-        def get_comment_author(process_id)
-          get_proposals(process_id).comments.map(&:author)
+        def get_comment_author
+          get_proposals.map(&:comments).flatten.map(&:author)
         end
 
-        def get_comment_followers(process_id)
-          up_votes = get_proposals(process_id).map(&:comments).flatten.map(&:up_votes).flatten.map(&:author)
-          down_votes = get_proposals(process_id).map(&:comments).flatten.map(&:down_votes).flatten.map(&:author)
+        def get_comment_followers
+          up_votes = get_proposals.map(&:comments).flatten.map(&:up_votes).flatten.map(&:author)
+          down_votes = get_proposals.map(&:comments).flatten.map(&:down_votes).flatten.map(&:author)
           up_votes + down_votes
         end
       end
